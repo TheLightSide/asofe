ddf9e18b613ec9cd19f9f69d0b2e0ab84acc7ee7
develnk <develnk@gmail.com>
2019-10-14 11:33:58 +0300

    Fixed calc fee. Fixed block download state Changed version

--- src/amount.cpp
+++ src/amount.cpp
@@ -19,12 +19,14 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nSize)
 
 CAmount CFeeRate::GetFee(size_t nSize) const
 {
-    CAmount nFee = nSatoshisPerK*nSize / 1000;
+  double calcSatoshi = static_cast<double>(nSatoshisPerK) * nSize / 100000;
+  CAmount nFee = (calcSatoshi < 1 && calcSatoshi != 0) ? 1 : calcSatoshi;
 
-    if (nFee == 0 && nSatoshisPerK > 0)
-        nFee = nSatoshisPerK;
+  if (nFee == 0 && nSatoshisPerK > 0) {
+    nFee = nSatoshisPerK;
+  }
 
-    return nFee;
+  return nFee;
 }
 
 std::string CFeeRate::ToString() const
